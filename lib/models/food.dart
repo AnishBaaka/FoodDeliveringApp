3adf39dff16aeec12e52a62601a6951b0590a978
AnishBaaka <anishwagle2@gmail.com>
2024-04-05 09:37:02 +0545

    added tabBar, food model and images

---
+++ lib/models/food.dart
@@ -0,0 +1,38 @@
+//food item
+class Food {
+  final String name;            //cheese burgers
+  final String description;     // a burger full of cheese
+  final String imagePath;       //lib/images/cheese_burger.png
+  final double price;           //$4.99
+  final FoodCategory category;  //burger
+  List<Addons> availableAddons; //[extra cheese,sauce,patty]
+
+  Food({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.price,
+    required this.category,
+    required this.availableAddons,
+  });
+}
+
+//Food categoreies
+enum FoodCategory {
+  burgers,
+  salads,
+  sides,
+  deserts,
+  drinks,
+}
+
+//food addons
+class Addons {
+  String name;
+  double price;
+
+  Addons({
+    required this.name,
+    required this.price,
+  });
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:newfood/components/my_current_location.dart';
 import 'package:newfood/components/my_description_box.dart';
 import 'package:newfood/components/my_drawer.dart';
 import 'package:newfood/components/my_silver_app_bar.dart';
+import 'package:newfood/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,36 +12,65 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  //tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MyDrawer(),
       body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          MySilverAppBar(
-            title: const Text("Title"),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Divider(
-                  indent: 25,
-                  endIndent: 25,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                //my current location
-                const MyCurrentLocation(),
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                MySilverAppBar(
+                  title: MyTabBar(tabController: _tabController),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Divider(
+                        indent: 25,
+                        endIndent: 25,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                      //my current location
+                      const MyCurrentLocation(),
 
-                //description box
-                const MyDescriptionBox()
+                      //description box
+                      const MyDescriptionBox()
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-        ],
-        body: Container(
-          color: Colors.blue,
-        ),
-      ),
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => const Text("First tab items"),
+              ),
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => const Text("Second tab items"),
+              ),
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => const Text("Third tab items"),
+              ),
+            ],
+          )),
     );
   }
 }
